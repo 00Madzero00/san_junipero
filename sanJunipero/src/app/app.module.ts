5adab9bbbbfc87d061d533a24bfeb85166f07876
Joe <joe.hardwidge@ntlworld.com>
2018-04-11 22:35:27 +0100

    Figuring out navbar

--- sanJunipero/src/app/app.module.ts
+++ sanJunipero/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, PLATFORM_ID, APP_ID, Inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 
@@ -15,26 +20,36 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'San Junipero' }),
+    FlexLayoutModule,
+    BrowserAnimationsModule,
     MatCardModule,
     MatButtonModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
   exports: [
+    FlexLayoutModule,
+    BrowserAnimationsModule,
     MatCardModule,
     MatButtonModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
 
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: Object,
-    @Inject(APP_ID) private appId: string) {
-    const platform = isPlatformBrowser(platformId) ?
-      'in the browser' : 'on the server';
-    console.log(`Running ${platform} with appId=${appId}`);
+  // constructor(
+  //   @Inject(PLATFORM_ID) private platformId: Object,
+  //   @Inject(APP_ID) private appId: string) {
+  //   const platform = isPlatformBrowser(platformId) ?
+  //     'in the browser' : 'on the server';
+  //   console.log(`Running ${platform} with appId=${appId}`);
     
-  }
+  // }
 }
