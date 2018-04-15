6fd98ea7bdb68a2bc0b07918efa945628b40a62c
Joe <joe.hardwidge@ntlworld.com>
2018-04-15 20:06:18 +0100

    Created responsive jumbotron on HomeComponent

--- sanJunipero/src/app/home/home.component.ts
+++ sanJunipero/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Inject, Component, OnInit, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(PLATFORM_ID) private platformId
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.handleJumbo();
+    }
+  }
+
+  public height;
+  public width;
+
+  private handleJumbo() {
+    const video = document.getElementsByClassName('jumbo');
+    const _self = this;
+
+    this.height = window.innerHeight - 100;
+    this.width = window.innerWidth;
+
+    window.addEventListener('resize', function() {
+      _self.height = this.innerHeight - 100;
+      _self.width = this.innerWidth;
+    });
+
+  }
 
   ngOnInit() {
   }
